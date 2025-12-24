4b97e43f5788c54e191f5cc881458e393aee80ba
EslamFouda7 <eslamamer311@gmail.com>
2025-12-24 16:51:19 +0200

    first commit

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,8 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: '', redirectTo: 'home', pathMatch: 'full' },
+    { path: 'home', loadComponent: () => import('./pages/landing/landing').then(c => c.Landing), },
+     { path: 'games', loadComponent: () => import('./pages/games/games').then(c => c.Games), },
+      { path: 'favorites', loadComponent: () => import('./pages/favorites/favorites').then(c => c.Favorites), },
+];
